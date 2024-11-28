8130ad4cdb8a9f5b3bc65b7f45f6a81e69005f35
Ricardo Roman Landeros <ricardomon85@gmail.com>
2024-11-28 15:07:30 -0600

    Initial Commit

---
+++ IPropinas/IPropinasApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPropinasApp.swift
+//  IPropinas
+//
+//  Created by Ricardo Roman Landeros on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct IPropinasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
